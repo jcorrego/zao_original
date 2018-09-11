164c2f7fe35af20a231cef971c2d2327508ee694
Juan Carlos Orrego <jcorrego@gmail.com>
2018-09-11 05:59:01 -0500

    Settings

---
+++ config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'wwwzaon_website');
+define('_DB_USER_', 'wwwzaon_website');
+define('_DB_PASSWD_', 'CO@2014sm');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'qeqSPx5uH8w9DzgmFUGHW0OSk090ZHhJOaiOVZsp5ahK7VxUcYbV1TgG');
+define('_COOKIE_IV_', 'IAHeru7v');
+define('_PS_CREATION_DATE_', '2017-03-08');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.5');
